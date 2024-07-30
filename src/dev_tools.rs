cacb4916aae7a16400bc1bb5b29f7a1108581c08
TinyGodzilla <kenlee7539@gmail.com>
2024-07-30 16:41:09 -0700

    it's functioning the way I am expecting to.

--- src/dev_tools.rs
+++ src/dev_tools.rs
@@ -2,12 +2,12 @@
 
 use bevy::prelude::*;
 use bevy::dev_tools::states::log_transitions;
-// use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 use crate::core::states;
 
 pub(super) fn plugin(app: &mut App) {
     // Print state transitions in dev builds
     app.add_systems(Update, log_transitions::<states::GameState>);
-    // app.add_plugins(WorldInspectorPlugin::new());
+    app.add_plugins(WorldInspectorPlugin::new());
 }
--- src/game/scenes/loading.rs
+++ src/game/scenes/loading.rs
@@ -1,4 +1,12 @@
 use bevy::prelude::*;
+use bevy::ui::Val::*;
+
+use crate::core::assets::{
+    HandleMap,
+    prelude::*
+};
+
+use crate::core::components::UiRoot;
 
 use bevy::input::common_conditions::input_just_pressed;
 
@@ -7,38 +15,131 @@ use crate::core::states::GameState;
 use crate::game::asset_loader;
 
 pub(super) fn plugin(app: &mut App) {
-    app.add_systems(OnEnter(GameState::Loading), (setup_loading_screen, asset_loader::load_assets));
+    app.add_systems(
+        OnEnter(GameState::Loading),
+        (setup_loading_ui, start_loading_asset),
+    );
+
+    app.add_systems(FixedUpdate, poll_asset_loaded.run_if(in_state(GameState::Loading).and_then(any_with_component::<AssetLoadChecker>)));
+
     app.add_systems(
         FixedUpdate,
-        continue_to_game.run_if(in_state(GameState::Loading).and_then(asset_loader::all_assets_loaded).and_then(input_just_pressed(KeyCode::Enter).or_else(input_just_pressed(KeyCode::Space)))),
+        continue_to_game.run_if(
+            in_state(GameState::Loading)
+                .and_then(asset_loader::all_assets_loaded)
+                .and_then(
+                    input_just_pressed(KeyCode::Enter).or_else(input_just_pressed(KeyCode::Space)),
+                ),
+        ),
     );
+}
+
+/********************************************************
+ *              For Asset Load Checking..
+ *  The general idea is that Spawn a Load Checker to run the polling system. Once it's loaded Trigger a system and unmount itself.
+********************************************************/
+#[derive(Component)]
+struct AssetLoadChecker;
+
+#[derive(Event)]
+struct AssetLoaded;
+
+fn start_loading_asset(mut commands: Commands) {
+    commands.spawn(AssetLoadChecker);
+    asset_loader::load_assets(commands);
+}
+
+fn poll_asset_loaded(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    sfx_handles: Res<HandleMap<SfxKey>>,
+    soundtrack_handles: Res<HandleMap<SoundtrackKey>>,
+    mesh_handles: Res<HandleMap<MeshKey>>,
+    loader_query: Query<Entity, With<AssetLoadChecker>>,
+    ui_root_query: Query<Entity, With<UiRoot>>,
+) {
+    if !asset_loader::all_assets_loaded(asset_server, sfx_handles, soundtrack_handles, mesh_handles) {
+        return;
+    }
+
+    let asset_load_checker = loader_query.single();
+    commands.entity(asset_load_checker).despawn();
+
+    commands.trigger_targets(AssetLoaded, ui_root_query.single());
 }
 
 fn continue_to_game(mut game_state: ResMut<NextState<GameState>>) {
     game_state.set(GameState::InGame);
 }
 
-fn setup_loading_screen(mut commands: Commands) {
+fn on_asset_loaded(_trigger: Trigger<AssetLoaded>, mut commands: Commands, mut query: Query<Entity, With<UiRoot>>) {
+    println!("loaded");
+    // Spawn Continue Text to UI Root
+    let ui_root = query.single_mut();
 
+    commands.entity(ui_root).with_children(|parent| {
+        parent.spawn((
+            Name::new("Header Text"),
+            TextBundle::from_section(
+                "Press [Space] or [Enter] To Continue",
+                TextStyle {
+                    font_size: 40.0,
+                    color: Color::srgb(0.867, 0.827, 0.412),
+                    ..default()
+                },
+            ),
+        ));
+    });
+}
+
+fn setup_loading_ui(mut commands: Commands) {
+    spawn_loading_ui(&mut commands);
     // Setup the UI
-    commands.spawn((
-        Name::new("Header Text"),
-        TextBundle::from_section(
-            "Loading",
-            TextStyle {
-                font_size: 40.0,
-                color: Color::srgb(0.867, 0.827, 0.412),
-                ..default()
-            },
-        ),
-        StateScoped(GameState::Loading)
-    ));
-
     // Spawn a Camera to show the UI Text.
     commands.spawn((
         Name::new("Camera"),
         Camera2dBundle::default(),
         IsDefaultUiCamera,
-        StateScoped(GameState::Loading)
+        StateScoped(GameState::Loading),
     ));
+}
+
+fn spawn_loading_ui(command: &mut Commands) {
+    let ui_root = command
+        .spawn((
+            Name::new("UI Root"),
+            UiRoot,
+            NodeBundle {
+                style: Style {
+                    width: Percent(100.0),
+                    height: Percent(100.0),
+                    justify_content: JustifyContent::Center,
+                    align_items: AlignItems::Center,
+                    flex_direction: FlexDirection::Column,
+                    row_gap: Px(10.0),
+                    position_type: PositionType::Absolute,
+                    ..default()
+                },
+                ..default()
+            },
+            StateScoped(GameState::Loading),
+        ))
+        .observe(on_asset_loaded)
+        .id();
+
+    let loading_text = command
+        .spawn((
+            Name::new("Header Text"),
+            TextBundle::from_section(
+                "Loading",
+                TextStyle {
+                    font_size: 40.0,
+                    color: Color::srgb(0.867, 0.827, 0.412),
+                    ..default()
+                },
+            ),
+        ))
+        .id();
+
+    command.entity(ui_root).push_children(&[loading_text]);
 }
